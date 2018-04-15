0e0c1005b7fb409b36b421dabeebe0e1b1ca5790
icemanzb <254784109@qq.com>
2018-04-15 21:19:15 +0800

    commit

--- 第三章：自定义滑屏课件/js/miaov.touch.js
+++ 第三章：自定义滑屏课件/js/miaov.touch.js
@@ -1,70 +1,69 @@
-function transform(el,attr,val){
-	if(!el.transform){
-		el.transform = {
-		};
+function transform(el, attr, val) {
+	if (!el.transform) {
+		el.transform = {};
 	}
-	if(val === undefined){
+	if (val === undefined) {
 		return el.transform[attr];
 	}
 	el.transform[attr] = val;
 	var str = "";
-	for(var s in el.transform){
-		switch(s){
+	for (var s in el.transform) {
+		switch (s) {
 			case "rotate":
 			case "rotateX":
 			case "rotateY":
 			case "rotateZ":
 			case "skewX":
 			case "skewY":
-				str += s +"("+el.transform[s]+"deg) ";
+				str += s + "(" + el.transform[s] + "deg) ";
 				break;
 			case "scale":
 			case "scaleX":
 			case "scaleY":
-				str += s +"("+el.transform[s]+") ";
+				str += s + "(" + el.transform[s] + ") ";
 				break;
 			case "translateX":
 			case "translateY":
 			case "translateZ":
-				str += s +"("+el.transform[s]+"px) ";
-				break;	
+				str += s + "(" + el.transform[s] + "px) ";
+				break;
 		}
 	}
 	el.style.WebkitTransform = el.style.transform = str;
 }
-function tap(el,fn){
+function tap(el, fn) {
 	var startPoint = {};
-	el.addEventListener('touchstart', function(e) {
+	el.addEventListener('touchstart', function (e) {
 		startPoint = {
 			x: e.changedTouches[0].pageX,
 			y: e.changedTouches[0].pageY
 		}
 	});
-	el.addEventListener('touchend', function(e) {
+	el.addEventListener('touchend', function (e) {
 		var nowPoint = {
 			x: e.changedTouches[0].pageX,
 			y: e.changedTouches[0].pageY
-		}
-		if(Math.abs(nowPoint.x - startPoint.x) < 5
-		&&Math.abs(nowPoint.y - startPoint.y) < 5){
-			fn&&fn.call(el,e);
+		};
+		if (Math.abs(nowPoint.x - startPoint.x) < 5
+			&& Math.abs(nowPoint.y - startPoint.y) < 5) {
+			fn && fn.call(el, e);
 		}
 	});
 }
-function css(el,attr,val){
-	var transformAttr = ["rotate","rotateX","rotateY","rotateZ","skewX","skewY","scale","scaleX","scaleY","translateX","translateY","translateZ"];
-	for(var i = 0; i < transformAttr.length; i++){
-		if(attr == transformAttr[i]){ //如果 attr 等transform其中一个值就代表用用户想要操作的是 transform
-			return transform(el,attr,val);
+function css(el, attr, val) {
+	var transformAttr = ["rotate", "rotateX", "rotateY", "rotateZ", "skewX", "skewY", "scale", "scaleX", "scaleY", "translateX", "translateY", "translateZ"];
+	for (var i = 0; i < transformAttr.length; i++) {
+		if (attr == transformAttr[i]) { // 如果 attr 等 transform 其中一个值就代表用用户想要操作的是 transform
+			return transform(el, attr, val);
 		}
 	}
-	if(val === undefined){
+	if (val === undefined) {
 		val = getComputedStyle(el)[attr];
 		console.log(val);
 		val = parseFloat(val);
 		return val;
 	}
-	if(attr == "opacity"){
+	if (attr == "opacity") {
 		el.style[attr] = val;
 	} else {
 		el.style[attr] = val + "px";
